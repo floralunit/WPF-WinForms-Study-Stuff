607e019a9285f04bca2b480b7ff745874a30e771
Anna Yaskunova <anya71034@gmail.com>
2021-10-19 11:04:38 +0300

    18.10.21

---
+++ NetworkProgramming/WebResponse_3/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebResponse_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void openFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = txb_fileuri.Text;
+            FileWebRequest request =
+                   (FileWebRequest)WebRequest.Create(filename);
+
+            using (StreamReader sr = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                txb_fileContent.Text = sr.ReadToEnd();
+            }
+        }
+        private void writeFile_Click(object sender, RoutedEventArgs e)
+        {
+            WebRequest request = WebRequest.Create(txb_fileuri.Text);
+            request.Method = "PUT";
+            using (StreamWriter sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(txb_writefile.Text);
+            }
+        }
+        private void request_Click(object sender, RoutedEventArgs e)
+        {
+            // Создать объект запроса
+            WebRequest request = WebRequest.Create(txb_fileContent.Text);
+            request.BeginGetResponse(new AsyncCallback(OnResponse), request);
+        }
+
+        protected void OnResponse(IAsyncResult ar)
+        {
+            WebRequest request = (WebRequest)ar.AsyncState;
+            WebResponse response = request.EndGetResponse(ar);
+
+            // Читаем ответ
+}
+    }
+}
