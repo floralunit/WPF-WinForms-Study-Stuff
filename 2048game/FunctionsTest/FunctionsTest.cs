f36b3a4489b026c83d57c5274163fc8b3957b4d2
Anya-jpg <71765648+Anya-jpg@users.noreply.github.com>
2021-11-08 09:30:46 +0300

    Functions creation test

---
+++ 2048game/FunctionsTest/FunctionsTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FunctionsTest
+{
+    [TestClass]
+    public class FunctionsTest
+    {
+        [TestMethod]
+        public void functionsClassCreationTest()
+        {
+            Function function = new Function();
+            Assert.IsNotNull(function);
+        }
+    }
+}
---
+++ 2048game/FunctionsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FunctionsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FunctionsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32de1e66-99f6-4787-ab93-7d1cff506295")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
